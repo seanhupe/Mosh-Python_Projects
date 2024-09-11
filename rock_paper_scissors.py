e3d7d24b678a038d0f01a6360861e12e627c62c8
Sean Hupe <seanhupe@yahoo.com>
2024-09-10 20:10:03 -0700

    Update

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,19 +5,32 @@ emojis = {
     's': '✂️',
     'p': '📃',
 }
-
 choices = ('r', 'p', 's')
-user_choice = input("rock, paper, scissors? (r/p/s): ").lower()
 
-if user_choice not in choices:
-    print("Invalid selection!")
+while True:
+    user_choice = input("rock, paper, scissors? (r/p/s): ").lower()
+    if user_choice not in choices:
+        print("Invalid selection!")
+        continue
 
-computer_choice = random.choice(choices)
+    computer_choice = random.choice(choices)
 
-print(f"You chose {emojis[user_choice]}")
-print(f"Computer chose {emojis[computer_choice]}")
+    print(f"You chose {emojis[user_choice]}")
+    print(f"Computer chose {emojis[computer_choice]}")
 
+    if user_choice == computer_choice:
+        print("Tie!")
+    elif (
+            (user_choice == 'r' and computer_choice == 's') or
+            (user_choice == 's' and computer_choice == "p") or
+            (user_choice == 'p' and computer_choice == "r")):
+        print("You Win!")
+    else:
+        print("You LOSER!")
 
+    should_continue = input("Continue? (y/n): ").lower()
+    if should_continue == 'n':
+        break
 
 # Ask user to make a choice
 # if not valid
