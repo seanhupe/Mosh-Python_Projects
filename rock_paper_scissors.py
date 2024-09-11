6e96113d69f71a26465ebe33b1ed65abc29e6dc2
Sean Hupe <seanhupe@yahoo.com>
2024-09-10 17:37:16 -0700

    Update

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,30 @@
+import random
 
-choice = input("rock, paper, scissors? (r/p/s)")
+emojis = {
+    'r': '🪨',
+    's': '✂️',
+    'p': '📃',
+}
 
-print(f"You chose {choice}")
+choices = ('r', 'p', 's')
+user_choice = input("rock, paper, scissors? (r/p/s): ").lower()
+
+if user_choice not in choices:
+    print("Invalid selection!")
+
+computer_choice = random.choice(choices)
+
+print(f"You chose {emojis[user_choice]}")
+print(f"Computer chose {emojis[computer_choice]}")
+
+
+
+# Ask user to make a choice
+# if not valid
+#   print error
+# Let the computer make a choice
+# Print choices (emojis)
+# Determine winner
+# Ask if they want to play again
+# if not
+#   terminate
