2bed19effd724de6b8eb339320618aec0c725b1d
Sean Hupe <seanhupe@yahoo.com>
2024-09-10 20:32:08 -0700

    Update

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,36 +1,57 @@
 import random
 
-emojis = {
-    'r': '🪨',
-    's': '✂️',
-    'p': '📃',
+ROCK = 'r'
+SCISSORS = 's'
+PAPER = 'p'
+emojis = {ROCK: '🪨', SCISSORS: '✂️', PAPER: '📃',
 }
-choices = ('r', 'p', 's')
+choices = tuple(emojis.keys())
 
-while True:
-    user_choice = input("rock, paper, scissors? (r/p/s): ").lower()
-    if user_choice not in choices:
-        print("Invalid selection!")
-        continue
+# choices = ('r', 'p', 's')
 
-    computer_choice = random.choice(choices)
 
+def get_user_choice():
+    while True:
+        user_choice = input("rock, paper, scissors? (r/p/s): ").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid choice!")
+
+
+def display_choices(user_choice, computer_choice):
     print(f"You chose {emojis[user_choice]}")
     print(f"Computer chose {emojis[computer_choice]}")
 
+
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Tie!")
     elif (
-            (user_choice == 'r' and computer_choice == 's') or
-            (user_choice == 's' and computer_choice == "p") or
-            (user_choice == 'p' and computer_choice == "r")):
+            (user_choice == ROCK and computer_choice == SCISSORS) or
+            (user_choice == SCISSORS and computer_choice == PAPER) or
+            (user_choice == PAPER and computer_choice == ROCK)):
         print("You Win!")
     else:
         print("You LOSER!")
 
-    should_continue = input("Continue? (y/n): ").lower()
-    if should_continue == 'n':
-        break
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        should_continue = input("Continue? (y/n): ").lower()
+        if should_continue == 'n':
+            break
+
+
+play_game()
 
 # Ask user to make a choice
 # if not valid
